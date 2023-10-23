de47aadf85c35811aef9c25025d6c7fb9282b1b1
ftakematsu <ftakematsu@gmail.com>
2023-10-23 20:51:05 -0300

    Token interceptor adjust

--- src/app/modules/login/services/auth.service.ts
+++ src/app/modules/login/services/auth.service.ts
@@ -13,6 +13,18 @@ export class AuthService {
     this.urlAPI = "http://127.0.0.1:8000/api/"
   }
 
+  setAccessToken(token: string): void {
+    localStorage.setItem('access_token', token);
+  }
+
+  getAccessToken(): string {
+    return localStorage.getItem('access_token')!;
+  }
+
+  clearAccessToken(): void {
+    localStorage.removeItem('access_token');
+  }
+
   login(usuario: string, senha: string): Observable<any> {
     return this.http.post<any>(this.urlAPI + "auth", {
       login: usuario,
