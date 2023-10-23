0d40cd899af618a3528ec05b8d5dc4e35d0fb32a
ftakematsu <ftakematsu@gmail.com>
2023-10-23 19:49:16 -0300

    token interceptor

---
+++ src/app/core/token-interceptors.ts
@@ -0,0 +1,30 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { AuthService } from "../shared/services/auth.service";
+import { Observable } from "rxjs";
+import { addToken } from "../config/http-header.const";
+
+@Injectable()
+export class TokenInterceptor implements HttpInterceptor {
+    private bypassUris = [
+        '/login',
+        '/auth', 
+    ];
+
+    constructor(private authService: AuthService) {}
+
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        if (this._isBypassUrls(req.url)) {
+          return next.handle(req);
+        }
+        const token = this.authService.getAccessToken();
+        req = addToken(req, token);
+        return next.handle(req);
+    }
+
+      
+    private _isBypassUrls(url: string): boolean {
+        return this.bypassUris.some((uri) => url.includes(uri));
+    }
+
+}
